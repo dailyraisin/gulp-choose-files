e069db9f2b20644d861f44e05f8615edad57264d
Joe K <joe@dailyraisin.com>
2018-07-10 15:09:35 -0400

    cleanup

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ module.exports = function(options) {
   var files = {};
 
   return through.obj(function transform (file, enc, next) {
-    //var stream = this;
-
-    if (file.isNull()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Null not supported.'));
-      return next(null, file);
-    }
-
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
-
-      // or, if you can handle Streams:
-      //file.contents = file.contents.pipe(...
-      //return next(null, file);
-    }
-    else if (file.isBuffer()) {
-      //this.emit('info', 'my file path=' + file.path);
-    }
-
     if (opts.skip) {
       next(null, file);
       return;
@@ -61,13 +43,7 @@ module.exports = function(options) {
       return;
     }
 
-    //next(null, file);
-    //paths.forEach(function (filepath) {
-    //  stream.push(filepath);
-    //});
-
     var answers = {};
-    //stream.emit('info', 'possible files' + JSON.stringify(files));
 
     var prompt = new Prompt({
       name: 'files',
@@ -80,10 +56,8 @@ module.exports = function(options) {
 
     prompt.run(answers)
       .then(function(answers) {
-
         answers.forEach(function(filepath) {
           var myFile = files[filepath.name];
-          //stream.emit('info', 'chosen filepath...' + myFile + JSON.stringify(filepath));
           if (myFile !== undefined) {
             stream.push(myFile);
           }
@@ -91,27 +65,6 @@ module.exports = function(options) {
         next();
       })
       .catch(next);
-
-
-    /*prompt.run()
-      .then(function resolution (answers) {
-        //stream.emit('error', new PluginError(PLUGIN_NAME, 'hello choices'));
-
-        answers.forEach(function (filepath) {
-          stream.push(files[filepath]);
-          stream.emit('info', 'I want to use=' + filepath);
-        });
-      }, function rejection (rejection) {
-        stream.emit('error', new PluginError(PLUGIN_NAME, rejection));
-        next(rejection);
-      //  //return stream;
-      }).catch(function (e) {
-
-        stream.emit('error', new PluginError(PLUGIN_NAME, 'catch'));
-        //next();
-
-        //return stream;
-      });*/
   });
 };
 
